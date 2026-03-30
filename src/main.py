a71935da4cdca87615196bd75973a1f606954d1e
avgtrash <codysoloveoff@gmail.com>
2026-03-30 10:18:28 -0700

    Started crossover and mutation
    * MUST FINISH
    * MAKE EACH GIVE 6 CHILDREN

--- src/main.py
+++ src/main.py
@@ -1,16 +1,42 @@
 from src.utils.io_handler import load_arrangements
 from src.constants import PICKLE
+import random
+import numpy
 
 GENERATIONS = 1000
 POPULATION_SIZE = 95000
 
+#Uniform crossover - FINISH LATER
+#Give 6 children in return
+def crossover(parentA, parentB):
+    #Child that we are returning later
+    child_genome = []
 
-def crossover():
-    pass
+    for a,b in zip(parentA, parentB):
+        #"Coin flip" to decide crossover
+        #0 - Grab from A, 1 - Grab from B
+        RAND_NUMBER = random.randint(0, 1)
+        if(RAND_NUMBER == 0):
+            child_genome.append(a)
+        else:
+            child_genome.append(b)
+            
+    return child_genome
 
-
+#FINISH LATER
+#Give 6 children in return
 def mutate():
-    pass
+    data_set = load_arrangements(PICKLE)
+    data_set_genome = []
+    MUTATION_RATE = 0.05
+
+    #Select a random point and alter it
+    for i, a in zip(data_set):
+        rand_number = random.random()
+        if(MUTATION_RATE > rand_number):
+            data_set_genome.append(a)
+            data_set_genome[i] += 1
+    return data_set_genome
 
 
 def tournament(participants):
