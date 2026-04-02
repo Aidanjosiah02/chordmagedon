bbb5d6e1d6f4f87c5d6a33a7c5afb60e6fea87ab
Marvellous <ifezueyoung@gmail.com>
2026-04-02 14:57:01 -0700

    WIPGA

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from src.utils.io_handler import load_pickle
-from src.constants import ARRANGEMENT_PICKLE, GENERATIONS, PROCESSED_DIR, MARKOV_PICKLE_SUFFIX, MUTATION_SIZE, MUTATION_RATE
+from src.constants import ARRANGEMENT_PICKLE, GENERATIONS, PROCESSED_DIR, MARKOV_PICKLE_SUFFIX, MUTATION_RATE, POPULATION_SIZE
 import numpy as np
 import random
 
@@ -19,7 +19,7 @@ def crossover(parentA, parentB):
                 child_genome.append(a)
             else:
                 child_genome.append(b)
-    children.append(child_genome)
+        children.append(child_genome)
     return children
 
 def mutate(genome):
@@ -37,8 +37,16 @@ def mutate(genome):
 
 
 def tournament(participants):
+    if len(participants) < 2:
+        return [None, None]
+
     total_fitness = sum([participant.fitness for participant in participants])
+
+    if total_fitness <= 0:
+        return [None, None]
+
     fitnesses = [participant.fitness/total_fitness for participant in participants]
+
     return np.random.choice(participants, size=2, replace=False, p=fitnesses)
 
 
@@ -48,6 +56,7 @@ markov = load_pickle(PROCESSED_DIR / f"order2_{MARKOV_PICKLE_SUFFIX}")
 for i in range(GENERATIONS):
     new_population = []
     rejects = []
+
     # We need to call the eval fitness function to calculate
     # the fitness functions for all the members
     for individual in population:
@@ -55,16 +64,24 @@ for i in range(GENERATIONS):
 
     # Select parents to crossover / mutate tournament style
     # trying groups of 8 at first
-    while len(population) > 8:
-        competitors = random.sample(population, 8)
+
+    random.shuffle(population)
+    for j in range(0, len(population), 8):
+        competitors = population[j:j+8]
         parent1, parent2 = tournament(competitors)
+
+        if parent1 is None or parent2 is None:
+            continue
+
         new_population.append(parent1)
         new_population.append(parent2)
-        # remove the competitors from the population
-        [population.remove(item) for item in competitors]
-        competitors.remove(parent1)
-        competitors.remove(parent2)
-        rejects.extend(competitors)
-        print(len(population))
 
+        # children = crossover(parent1, parent2)
+        # new_population.extend(children)
+
+        rejects.extend(competitors)
+
+    print(len(rejects))
+    new_population.extend(rejects[::POPULATION_SIZE-len(new_population)])
     population = new_population
+    print(f"Generation {i}")
