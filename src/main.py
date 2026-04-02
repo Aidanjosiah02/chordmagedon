afa8450f9190555a5644c863b870edd6219fe7a7
avgtrash <codysoloveoff@gmail.com>
2026-04-02 08:46:04 -0700

    Made crossover return an array of 6 children
    Marvellous asked me to. Idk why, ask him later.

--- src/main.py
+++ src/main.py
@@ -12,16 +12,20 @@ MUTATION_RATE = 0.05
 #Uniform crossover
 def crossover(parentA, parentB):
     #Child that we are returning later
-    child_genome = []
+    #Array we are storing our 6 children in? Idk ask Marvellous
+    children = []
+    for i range 6:
+        
+        child_genome = []
 
-    for a,b in zip(parentA, parentB):
-        #"Coin flip" to decide crossover
-        if random.random() < 0.5:
-            child_genome.append(a)
-        else:
-            child_genome.append(b)
-            
-    return child_genome
+        for a,b in zip(parentA, parentB):
+            #"Coin flip" to decide crossover
+            if random.random() < 0.5:
+                child_genome.append(a)
+            else:
+                child_genome.append(b)
+    children.append(child_genome)
+    return children
 
 def mutate(genome):
     data_set_genome = []
